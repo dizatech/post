0df3e093ee44ccb01f232dfe7f9d3ab260e46fe7
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2021-09-03 00:23:34 +0430

    Added getFromCategory method in post facade

--- src/Facades/PostFacade.php
+++ src/Facades/PostFacade.php
@@ -6,6 +6,7 @@ namespace Modules\Post\Facades;
  * @class \Modules\Post\Facades\PostFacade
  *
  * @method static array all(string $postType)
+ * @method static array getFromCategory(string $postType, integer $category_id, integer $count=NULL)
  *
  * @see \Dizatech\Post\Repositories\PostRepository
  */
--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Post\Repositories;
 
+use Illuminate\Database\Eloquent\Builder;
 use Modules\Post\Models\Post;
 
 class PostRepository
@@ -9,5 +10,20 @@ class PostRepository
     public function all($postType)
     {
         return Post::query()->where('post_type', $postType)->paginate(16);
+    }
+
+    public function getFromCategory($postType, $category_id, $count=NULL)
+    {
+        $query = Post::where('post_type', $postType)
+        ->where('publish_status', 'published')
+        ->whereHas('categories', function(Builder $query) use ($category_id){
+            $query->where('category_id', $category_id);
+        });
+        if( $count ){
+            return $query->take($count)->get();
+        }
+        else{
+            return $query->paginate();
+        }
     }
 }
