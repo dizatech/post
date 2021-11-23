d48790122fbccb3bb93339eb4dde6cc2a67caab6
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2021-11-23 15:09:23 +0330

    Added condition to enable/disable home/panel routing based on config

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,53 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Post\Models\Post;
 
-Route::group([
-    'namespace'  => 'Modules\Post\Http\Controllers',
-    'prefix'     => 'panel',
-    'middleware' => ['web', 'auth', 'verified']
-], function () {
-    Route::resource('page', 'PageController')->parameters([
-        'page'          => 'post'
-    ]);
-    Route::resource('article', 'ArticleController')->parameters([
-        'article'      => 'post'
-    ]);
-    Route::resource('news', 'NewsController')->parameters([
-        'news'         => 'post'
-    ]);
-    Route::resource('video', 'VideoController')->parameters([
-        'video'        => 'post'
-    ]);
-    Route::resource('articleCategory', 'ArticleCategoryController')->parameters([
-        'articleCategory'   => 'postCategory'
-    ]);
-    Route::resource('newsCategory', 'NewsCategoryController')->parameters([
-        'newsCategory' => 'postCategory'
-    ]);
-    Route::resource('videoCategory', 'VideoCategoryController')->parameters([
-        'videoCategory' => 'postCategory'
-    ]);
-    Route::delete('/post/delete_post_ajax/{post}', 'PostController@PostDeleteAjax');
-    Route::delete('/post/delete_post_category_ajax/{postCategory}', 'PostCategoryController@PostCategoryAjax');
-});
+if( config('dizatech_post.manage_panel_routes') ){
+    Route::group([
+        'namespace'  => 'Modules\Post\Http\Controllers',
+        'prefix'     => 'panel',
+        'middleware' => ['web', 'auth', 'verified']
+    ], function () {
+        Route::resource('page', 'PageController')->parameters([
+            'page'          => 'post'
+        ]);
+        Route::resource('article', 'ArticleController')->parameters([
+            'article'      => 'post'
+        ]);
+        Route::resource('news', 'NewsController')->parameters([
+            'news'         => 'post'
+        ]);
+        Route::resource('video', 'VideoController')->parameters([
+            'video'        => 'post'
+        ]);
+        Route::resource('articleCategory', 'ArticleCategoryController')->parameters([
+            'articleCategory'   => 'postCategory'
+        ]);
+        Route::resource('newsCategory', 'NewsCategoryController')->parameters([
+            'newsCategory' => 'postCategory'
+        ]);
+        Route::resource('videoCategory', 'VideoCategoryController')->parameters([
+            'videoCategory' => 'postCategory'
+        ]);
+        Route::delete('/post/delete_post_ajax/{post}', 'PostController@PostDeleteAjax');
+        Route::delete('/post/delete_post_category_ajax/{postCategory}', 'PostCategoryController@PostCategoryAjax');
+    });
+}
 
-Route::group([
-    'namespace' => 'Modules\Post\Http\Controllers',
-    'middleware' => ['web']
-], function () {
-    Route::get('/page/{slug}', 'PageController@userShow')->name('page.user_show');
+if( config('dizatech_post.manage_home_routes') ){
+    Route::group([
+        'namespace' => 'Modules\Post\Http\Controllers',
+        'middleware' => ['web']
+    ], function () {
+        //post type indices
+        Route::get(config('dizatech_post.news_archive_url'), 'NewsController@userIndex')->name('news.user_index');
+        Route::get(config('dizatech_post.article_archive_url'), 'ArticleController@userIndex')->name('articles.user_index');
+        Route::get(config('dizatech_post.video_archive_url'), 'VideoController@userIndex')->name('videos.user_index');
 
-    Route::get('/اخبار', 'NewsController@userIndex')->name('news.user_index');
-    Route::get('/اخبار' . '/{slug}', 'NewsController@userShow')->name('news.user_show');
-    Route::get('اخبار/دسته-بندی/' . '{slug}', 'NewsCategoryController@userShow')->name('news_category.user_show');
+        //post type singles
+        Route::get(config('dizatech_post.news_base_url') . '/{slug}', 'NewsController@userShow')->name('news.user_show');
+        Route::get(config('dizatech_post.article_base_url') . '/{slug}', 'ArticleController@userShow')->name('generic_show');
+        Route::get(config('dizatech_post.video_base_url') . '/{slug}', 'VideoController@userShow')->name('video.user_show');
+        Route::get(config('dizatech_post.page_base_url') . '/{slug}', 'PageController@userShow')->name('page.user_show');
 
-    Route::get('/مقالات', 'ArticleController@userIndex')->name('articles.user_index');
-    Route::get('/مقالات' . '/{slug}', 'ArticleController@userShow')->name('article.user_show');
-    Route::get('مقالات/دسته-بندی/' . '{slug}', 'ArticleCategoryController@userShow')->name('article_category.user_show');
-
-    Route::get('/ویدیو', 'VideoController@userIndex')->name('videos.user_index');
-    Route::get('/ویدیو' . '/{slug}', 'VideoController@userShow')->name('video.user_show');
-});
-
-
+        //post type catgories
+        Route::get('اخبار/دسته-بندی/' . '{slug}', 'NewsCategoryController@userShow')->name('news_category.user_show');
+        Route::get('مقالات/دسته-بندی/' . '{slug}', 'ArticleCategoryController@userShow')->name('article_category.user_show');
+    });
+}
